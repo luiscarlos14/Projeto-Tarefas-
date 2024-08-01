0495ef9dd16c852f7d3cbc9ff85c56189f95cdf9
Luís Carlos Barros <73847837+luiscarlos14@users.noreply.github.com>
2024-08-01 16:18:13 -0300

    Projeto Parcial

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,60 @@
+import { GetServerSideProps } from "next";
+import styles from "./styles.module.css";
+import Head from "next/head";
+
+import { getSession } from 'next-auth/react';
+import { Textarea } from "@/components/textarea";
+
+export default function Dashboard() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Meu painel de tarefas</title>
+      </Head>
+
+      <main className={styles.main}>
+        <section className={styles.content}>
+          <div className={styles.contentForm}>
+            <h1 className={styles.title}>
+                Qual sua tarefa?
+            </h1>
+            <form>
+                <Textarea 
+                  placeholder="Digite qual a sua tarefa..."
+                />
+                <div className={styles.checkboxArea}>
+                    <input 
+                      type="checkbox" 
+                      className={styles.checkbox}
+                    />
+                    <label>Deixar tarefa pública?</label>
+                </div>
+
+                <button className={styles.button} type="submit">
+                    Registrar
+                </button>
+            </form>
+          </div>
+        </section>
+      </main>
+
+    </div>
+  );
+}
+
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const session = await getSession({ req });
+
+  if (!session?.user) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false
+      }
+    }
+  }
+  return {
+    props: {}
+  }
+}
